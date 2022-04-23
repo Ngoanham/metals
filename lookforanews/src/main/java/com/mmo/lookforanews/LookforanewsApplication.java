7387457ed803cc2083e6bb097e438d461aa1caf8
Ngoanham <97075225+Ngoanham@users.noreply.github.com>
2022-04-23 15:03:32 +0700

    add new commit
    add new commit

---
+++ lookforanews/src/main/java/com/mmo/lookforanews/LookforanewsApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforanews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookforanewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookforanewsApplication.class, args);
+	}
+
+}
---
+++ lookforanews/src/test/java/com/mmo/lookforanews/LookforanewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforanews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookforanewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
